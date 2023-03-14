b72a03a3996f694de84394c79c7e243204e0dd1d
Taylor Threader <44672093+LunaTMT@users.noreply.github.com>
2023-03-14 17:43:19 +0000

    Add files via upload

---
+++ 10.py
@@ -0,0 +1,15 @@
+def get_primes(num):
+    prime = [True for i in range(num+1)]
+
+    p = 2
+    while (p ** 2 <= num):
+
+        if (prime[p] == True):
+
+            for i in range(p ** 2, num+1, p):
+                prime[i] = False
+        p += 1
+    
+    return [p for p in range(2, num+1) if prime[p]]
+
+print(sum(get_primes(2000000)))
